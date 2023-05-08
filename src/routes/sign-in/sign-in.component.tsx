41053b9ffdadce2741ff4ccd52dfb6dfa2a30008
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-05-08 20:47:59 +0100

    Added firebase and related utils

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,7 +1,20 @@
 import { FC, ReactElement } from 'react';
+import {
+  signInWIthGoogle,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn: FC = (): ReactElement => {
-  return <div>SignIn</div>;
+  const logGoogleUser = async () => {
+    const { user } = await signInWIthGoogle();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <button onClick={logGoogleUser}>Sign In With Google</button>
+    </div>
+  );
 };
 
 export default SignIn;
---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,71 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  User,
+} from 'firebase/auth';
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  QueryDocumentSnapshot,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA64XEr_P-on7A-w7dmksRVnWsCwzNo4DY',
+  authDomain: 'random-meal-platform.firebaseapp.com',
+  projectId: 'random-meal-platform',
+  storageBucket: 'random-meal-platform.appspot.com',
+  messagingSenderId: '707026385488',
+  appId: '1:707026385488:web:a408c9b694b48e76b46f55',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(firebaseApp);
+export const signInWIthGoogle = () => signInWithPopup(auth, provider);
+
+// FireStore Config
+export const db = getFirestore();
+
+export type UserDocument = {
+  displayName: string;
+  email: string;
+  createdAt: Date;
+};
+
+export const createUserDocumentFromAuth = async (
+  userAuth: User,
+): Promise<QueryDocumentSnapshot<UserDocument> | void> => {
+  // db, collection, id: receive a doc instance
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  // use the doc instance to check if the user exists
+  const userDocSnapshot = await getDoc(userDocRef);
+  const isUserDocExists = userDocSnapshot.exists();
+
+  if (!isUserDocExists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (e) {
+      console.log('Error creating user', e);
+    }
+  }
+  return userDocSnapshot as QueryDocumentSnapshot<UserDocument>;
+};
