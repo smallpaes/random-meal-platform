8c4ecba61e4f15a366f8b2bc5166bfa549bde862
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-05-21 15:50:10 +0100

    Added support for browser redux dev tool

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, Middleware, compose } from 'redux';
 import logger from 'redux-logger';
 import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -6,6 +6,12 @@ import storage from 'redux-persist/lib/storage';
 import { rootReducer } from './root-reducer';
 
 export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
 type ExtendedPersistConfig = PersistConfig<RootState> & {
   blacklist: (keyof RootState)[];
@@ -19,10 +25,19 @@ const persistConfig: ExtendedPersistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const middleware = [process.env.NODE_ENV !== 'production' && logger].filter(
+  (middleware): middleware is Middleware => Boolean(middleware),
+);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
 export const store = createStore(
   persistedReducer,
   undefined,
-  applyMiddleware(logger),
+  composeEnhancer(applyMiddleware(...middleware)),
 );
 
 export const persistor = persistStore(store);
