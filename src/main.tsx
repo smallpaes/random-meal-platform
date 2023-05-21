893df6e9b7a246d0b2176b57d9b1de74ad9e0f72
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-05-21 15:13:26 +0100

    Added redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,24 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import * as ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
+import { PersistGate } from 'redux-persist/integration/react';
 import { theme } from './theme';
 import { GlobalStyles } from './theme/globalStyles';
 import App from './App.tsx';
-import { store } from './store/store.ts';
+import { store, persistor } from './store/store.ts';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,28 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
 
 export type RootState = ReturnType<typeof rootReducer>;
 
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  blacklist: (keyof RootState)[];
+};
+
+const persistConfig: ExtendedPersistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-  rootReducer,
+  persistedReducer,
   undefined,
   applyMiddleware(logger),
 );
+
+export const persistor = persistStore(store);
