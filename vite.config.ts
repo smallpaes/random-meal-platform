2392a11c2ee6740afa2433511d2957a5628658a7
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-05-24 15:50:05 +0100

    Updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    base: '/',
-    plugins: [svgr(), react()],
-  };
-
-  if (command !== 'serve') {
-    config.base = '/random-meal-platform/';
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [svgr(), react()],
 });
