be71c5e10843449304a6107c94e12092bde28d77
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-05-24 15:38:57 +0100

    Updated vite config file to serve different base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/random-meal-platform/',
-  plugins: [svgr(), react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    base: '/',
+    plugins: [svgr(), react()],
+  };
+
+  if (command !== 'serve') {
+    config.base = '/random-meal-platform/';
+  }
+
+  return config;
 });
