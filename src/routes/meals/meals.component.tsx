2ab464741ac384ab8e6bb522fe52f284de27c51d
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-05-27 17:29:54 +0100

    Removes unused import

--- src/routes/meals/meals.component.tsx
+++ src/routes/meals/meals.component.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactElement, useEffect } from 'react';
+import { FC, ReactElement } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import MealsPreview from '../categories-preview/categories-preview.component';
